7814ae96f2201b5bdfc50a0c2700e83d1835fed7
Matt Smith <mrandolph83@gmail.com>
2020-09-08 15:30:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,11 +47,19 @@ def turn(board)
 end
 
 def turn_count(board)
-  turn = 0
-  board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
+  counter = 0
+  board.each do |player|
+    if player == "X" || player == "O"
+      counter += 1
     end
   end
-  return turn
+  counter
 end
+
+ def current_player(board)
+   if turn_count(board) % 2 == 0
+     return "X"
+   else
+      return "O"
+    end
+ end
